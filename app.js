ab3e8a66745b058664102b39cfa21f8ed16354ae
zolantal <zoltan_antal@outlook.com>
2024-01-04 16:45:08 +0000

    Complete exercise 11.11 - test error #2

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  throw 'error...'
+  process.exit(1)
   // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
